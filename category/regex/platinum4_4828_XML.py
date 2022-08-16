c47102783e1fce2e9e5d6f8ee7430161e2b3d47e
neltia <dsz08082@naver.com>
2022-08-16 23:29:07 +0900

    코드 정리

--- category/regex/platinum4_4828_XML.py
+++ category/regex/platinum4_4828_XML.py
@@ -13,16 +13,16 @@ def isvalid(data):
         return "valid"
 
     # escape
-    pat = r"(&amp;)|(&lt;)|(&gt;)"
-    data = re.sub(pat, "", data)
-
-    # 16진수 문자 제거
-    pat1 = r"&x([A-Fa-f0-9]{2})+\;"
+    pat1 = r"(&amp;)|(&lt;)|(&gt;)"
     data = re.sub(pat1, "", data)
 
+    # 16진수 문자 제거
+    pat2 = r"&x([A-Fa-f0-9]{2})+\;"
+    data = re.sub(pat2, "", data)
+
     # 태그 파싱
-    pat5 = r"\<[a-z0-9]+\>|\<\/[a-z0-9]+\>"
-    tags = re.findall(pat5, data)
+    pat3 = r"\<[a-z0-9]+\>|\<\/[a-z0-9]+\>"
+    tags = re.findall(pat3, data)
     # print("DATA1:", data)
     # print("TAGS:", tags)
 
@@ -43,12 +43,14 @@ def isvalid(data):
             context.append(tag)
         # print("CONTEXT:", context)
 
+    # <tag/> 닫는 태그 먼저 정리
     pat4 = r"<[a-z0-9]+\/\>"
     data = re.sub(pat4, "", data)
 
-    # 단일 문자열이 포함되면 안 됨: <, >, &
-    data = re.sub(pat5, "", data)
+    # <tag>, </tag> 일반 태그 정리
+    data = re.sub(pat3, "", data)
 
+    # 단일 문자열이 포함되면 안 됨: <, >, &
     strset = ["<", ">", "&"]
     for strs in strset:
         if strs in data:
@@ -57,8 +59,8 @@ def isvalid(data):
     # print("DATA2:", data)
 
     # plain text
-    pat = r"[\x20-\x7F]+"
-    data = re.sub(pat, "", data)
+    pat5 = r"[\x20-\x7F]+"
+    data = re.sub(pat5, "", data)
     # print("DATA3:", data)
     if len(data) != 0:
         return "invalid"
