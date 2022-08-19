da2451cd110c7dd53489dacf0ce79bb5a3494cbe
neltia <dsz08082@naver.com>
2022-08-19 19:55:17 +0900

    9095 - 재귀 -> dp 수정

--- dynamic_programming/silver3_9095_1,2,3 더하기.py
+++ dynamic_programming/silver3_9095_1,2,3 더하기.py
@@ -1,22 +1,26 @@
-"""
-solved: 22.08.14
-"""
-
 import sys
-
-nums = [1, 2]
 
 
 def sol(n):
-    if n in nums:
+    dp = list()
+    dp = [0]*(n+1)
+
+    if n in [1, 2]:
         return n
     elif n == 3:
         return 4
-    return sol(n-1) + sol(n-2) + sol(n-3)
+
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    for i in range(4, n+1):
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    return dp[n]
 
 
 t = int(input())
 for x in range(t):
-    dp = list()
     n = int(sys.stdin.readline())
     print(sol(n))
